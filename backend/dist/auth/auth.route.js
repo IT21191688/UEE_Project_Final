2931fc0b45b60931c7ffd2fafdaa022624077bc3
IT21191688 <IT21191688@my.sliit.lk>
2023-10-10 09:57:41 +0530

    push

--- backend/dist/auth/auth.route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_controller_1 = require("./auth.controller");
-const AuthRouter = (0, express_1.Router)();
-AuthRouter.post("/login", auth_controller_1.UserLogin);
-exports.default = AuthRouter;
