a27045cf6a26be749c85c217eb7bfe0dc57f8598
IT21191688 <IT21191688@my.sliit.lk>
2023-10-17 13:04:11 +0530

    commit

--- backend/src/util/cronJob.ts
+++ backend/src/util/cronJob.ts
@@ -15,7 +15,7 @@ export const cronJob = (cronTime: string, callback: () => void) => {
 
 //send appointment reminders daily at 6 am
 const sendAppointmentReminders = () => {
-  cronJob("0 6 * * *", async () => {
+  cronJob("* * * * *", async () => {
     console.log("Appointment reminder cron job running...");
     //get today's date
     const today = new Date();
