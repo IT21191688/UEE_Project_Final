5b7243c703b48bd66d24795683b3dd24f039c7d2
IT21071652 <it21071652@my.sliit.lk>
2023-10-25 09:24:58 +0530

    ViewNews

--- frontend/screens/NewsView.js
+++ frontend/screens/NewsView.js
@@ -2,16 +2,22 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, FlatList, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+//import { useNavigation } from '@navigation/native'; // Please adjust the import path to match your project's setup.
 import { useNavigation } from '@react-navigation/native';
+
 
 const NewsView = () => {
   const [newsData, setNewsData] = useState([]);
   const [error, setError] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
+  
   const navigation = useNavigation();
   const handleNavigate = () => {
     navigation.navigate('CreateNews');
   };
+
+  
 
   const handleNavigateUpdate = (id) => {
     navigation.navigate('UpdateNews', { newsId: id });
@@ -28,10 +34,6 @@ const NewsView = () => {
       const headers = {
         Authorization: `Bearer ${token}`,
       };
-
-
-      console.log('Deleting news item with ID:', newsId);
-
 
       const response = await axios.delete(`https://uee123.onrender.com/api/v1/news/deleteNews/${newsId}`, { headers });
 
@@ -74,8 +76,26 @@ const NewsView = () => {
     }
   };
 
-  return (
+  const filterNewsByCategory = (category) => {
+    setSelectedCategory(category);
+  };
 
+  // Filter the news based on the selected category
+  const filteredNews = selectedCategory
+    ? newsData.filter((item) => item.category.name === selectedCategory)
+    : newsData;
+
+    const localNewsButtonStyle = {
+      ...styles.categoryButton,
+      backgroundColor: selectedCategory === 'Local News' ? 'green' : '#007bff',
+    };
+
+    const eventsButtonStyle = {
+      ...styles.categoryButton,
+      backgroundColor: selectedCategory === 'Events' ? 'green' : '#007bff',
+    };
+
+  return (
     <View style={styles.container}>
       <TouchableOpacity style={styles.newAppointment} onPress={handleNavigate}>
         <Text style={styles.buttonTextAdd}>View News</Text>
@@ -83,36 +103,48 @@ const NewsView = () => {
       {error ? (
         <Text style={styles.errorText}>{error}</Text>
       ) : (
-        <FlatList
-          data={newsData}
-          keyExtractor={(item) => item._id}
-          renderItem={({ item }) => (
-            <View style={styles.newsItem}>
-              <Image
-                source={{ uri: item.newsImage }}
-                style={styles.newsImage}
-              />
-              <Text style={styles.category}>{item.category.name}</Text>
-              <Text style={styles.title}>{item.title}</Text>
-              <Text style={styles.content}>{item.content}</Text>
-
-              <View style={styles.buttonContainer}>
-                <TouchableOpacity
-                  style={styles.updateButton}
-                  onPress={() => handleNavigateUpdate(item._id)}
-                >
-                  <Text style={styles.buttonText}>Update</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={styles.deleteButton}
-                  onPress={() => handleDeleteNews(item._id)}
-                >
-                  <Text style={styles.buttonText}>Delete</Text>
-                </TouchableOpacity>
+        <View>
+          <View style={styles.buttonContainer}>
+          <TouchableOpacity
+          style={localNewsButtonStyle}
+          onPress={() => filterNewsByCategory('Local News')}
+        >
+          <Text style={styles.buttonText}>Local News</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+          style={eventsButtonStyle}
+          onPress={() => filterNewsByCategory('Events')}
+        >
+          <Text style={styles.buttonText}>Events</Text>
+        </TouchableOpacity>
+          </View>
+          <FlatList
+            data={filteredNews}
+            keyExtractor={(item) => item._id}
+            renderItem={({ item }) => (
+              <View style={styles.newsItem}>
+                <Image source={{ uri: item.newsImage }} style={styles.newsImage} />
+                <Text style={styles.category}>{item.category.name}</Text>
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.content}>{item.content}</Text>
+                <View style={styles.buttonContainer}>
+                  <TouchableOpacity
+                    style={styles.updateButton}
+                    onPress={() => handleNavigateUpdate(item._id)}
+                  >
+                    <Text style={styles.buttonText}>Update</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={styles.deleteButton}
+                    onPress={() => handleDeleteNews(item._id)}
+                  >
+                    <Text style={styles.buttonText}>Delete</Text>
+                  </TouchableOpacity>
+                </View>
               </View>
-            </View>
-          )}
-        />
+            )}
+          />
+        </View>
       )}
     </View>
   );
@@ -121,10 +153,10 @@ const NewsView = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
+    padding: 18,
   },
   newsItem: {
-    marginBottom: 16,
+    marginBottom: 50,
   },
   category: {
     fontSize: 18,
@@ -156,13 +188,36 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   updateButton: {
-    // Define styles for the "Update" button
+    backgroundColor: '#007bff',
+    borderRadius: 20,
+    padding: 12,
+    alignItems: 'center',
+    margin: 10,
+
+    
   },
   deleteButton: {
-    // Define styles for the "Delete" button
+
+    backgroundColor: '#007bff',
+    borderRadius: 20,
+    padding: 12,
+    alignItems: 'center',
+    margin: 10,
+ 
   },
   buttonText: {
     // Define styles for the button text
+  },
+  categoryButton: {
+    backgroundColor: '#007bff',
+    borderRadius: 20,
+    padding: 12,
+    alignItems: 'center',
+    margin: 8,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
 
