df6cf49e933c860051d5dd734cb6b513ef824c90
IT21071652 <it21071652@my.sliit.lk>
2023-10-27 21:58:03 +0530

    MyNews

--- frontend/screens/MyNews.js
+++ frontend/screens/MyNews.js
@@ -1,300 +1,315 @@
-import * as React from "react";
-import { ScrollView, StyleSheet, Text, View, TextInput } from "react-native";
-import { Image } from "expo-image";
-import NewsCard from "../components/NewsCard";
-import Property1Unselected from "../components/Property1Unselected";
-import { Color, FontFamily, FontSize, Border, Padding } from "../GlobalStyles";
+import axios from 'axios'; // Import the Axios library
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, Alert, Image,StyleSheet,ScrollView,TouchableOpacity } from 'react-native';
+import * as DocumentPicker from 'expo-document-picker';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Picker } from "@react-native-picker/picker";
+import { useNavigation } from '@react-navigation/native';
 
 const MyNews = () => {
+  const navigation = useNavigation();
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('64eaf5224b77b2ddf24cfabc');
+  const [content, setContent] = useState('');
+  const [newsImage, setNewsImage] = useState(null);
+
+  const selectFile = async () => {
+    const result = await DocumentPicker.getDocumentAsync({ type: 'image/*' });
+
+    if (result.type === 'success') {
+      setFile(result);
+    } else {
+      Alert.alert('File selection canceled');
+    }
+  };
+ 
+
+
+  const uploadNews = async () => {
+    if (!file) {
+      Alert.alert('Please select a file first');
+      return;
+    }
+
+    const token = await AsyncStorage.getItem('token');
+
+    if (!token) {
+      console.error('Token is missing in AsyncStorage');
+      return;
+    }
+    console.log(token)
+
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('category', category);
+    formData.append('content', content);
+    formData.append('file', {
+      name: file.name,
+      type: file.type,
+      uri: file.uri,
+    });
+
+    try {
+      const response = await axios.post('http://192.168.43.93:8090/api/v1/news/create', formData, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      if (response.status === 201) {
+        const data = response.data;
+        if (data.success) {
+          Alert.alert('News uploaded successfully');
+        } else {
+          Alert.alert('News upload failed');
+        }
+      } else {
+        Alert.alert('Server Error', 'Failed to upload news: ' + response.data.message);
+      }
+    } catch (error) {
+      Alert.alert('Error', 'Failed to upload news: ' + error.message);
+    }
+  };
+
+  const handleViewNews = () => {
+    // Use navigation.navigate to navigate to the "NewsView" screen
+    navigation.navigate('NewsView');
+  };
+
+  const handleAddAppointment = async () => {
+  
+
+    try {
+      const token = await AsyncStorage.getItem('token');
+      if (!token) {
+        console.error('Token is missing in AsyncStorage');
+        return;
+      }
+      const headers = {
+        'Authorization': `Bearer ${token}`,
+      };
+
+      // Create a request body object with the appointment details
+      const requestBody = {
+        title: title,
+        category: category, // Replace with your description
+        content: content,
+        newsImage: newsImage, // Make sure selectedTimeSlot is set to a valid value
+
+      };
+
+      console.log(requestBody)
+      const response = await axios.post(
+        'https://uee123.onrender.com/api/v1/news/create',
+        requestBody,
+        { headers }
+      );
+
+      if (response.data.isSuccessful) {
+        Alert.alert("Successfully Created");
+        //navigation.navigate("AppoinmentSuccess")
+      } else {
+        Alert.alert("Failed Try Again: " + response.data.message);
+        //navigation.navigate("Appointments")
+      }
+    } catch (error) {
+      //console.error('Error creating', error);
+      Alert.alert("Failed to Create" + error.message);
+    }
+  };
+
+  const HomeScreen = () => {
+    const navigation = useNavigation();
+  
+    const handleAddAppointment = () => {
+      navigation.navigate('NewsView'); // Navigate to the NewsView screen
+    }
+
+  };
+
   return (
-    <View style={styles.myNews}>
-      <ScrollView
-        style={[styles.scrollableSection, styles.scrollableSectionPosition]}
-        horizontal={false}
-        showsVerticalScrollIndicator={false}
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.scrollableSectionScrollViewContent}
-      >
-        <NewsCard
-          articleImageUrl={require("../assets/rectangle-4.png")}
-          newsImageUrl="Philippines' military conducts successful operations against Islamist extremist groups"
-          newsTitle="Rappler"
-          imageDimensions={require("../assets/ellipse-11.png")}
-          newsDescription="rpl"
-          timeAgo="2 hours ago"
-        />
-        <NewsCard
-          articleImageUrl={require("../assets/rectangle-41.png")}
-          newsImageUrl="Leading tech company unveils latest innovation in artificial intelligence (A1) - Takeaways"
-          newsTitle="Sunrise Scoops"
-          imageDimensions={require("../assets/ellipse-12.png")}
-          newsDescription="srs"
-          timeAgo="5 hours ago"
-          philippinesMilitaryConducMarginTop={13}
-          rapplerTop={196}
-          fmIconLeft={2}
-          fmIconTextAlign="center"
-        />
-        <NewsCard
-          articleImageUrl={require("../assets/rectangle-42.png")}
-          newsImageUrl="Philippines' business sector sees positive growth as lockdown measures ease"
-          newsTitle="Sureshot News"
-          imageDimensions={require("../assets/ellipse-13.png")}
-          newsDescription="sn"
-          timeAgo="a day ago"
-          philippinesMilitaryConducMarginTop={13}
-          rapplerTop={196}
-          fmIconLeft={5}
-          fmIconTextAlign="center"
-        />
-      </ScrollView>
-      {/* <Image
-        style={styles.menuBarIcon}
-        contentFit="cover"
-        source={require("../assets/menu-bar3.png")}
-      /> */}
-      <Image
-        style={[styles.myNewsChild, styles.searchPosition]}
-        contentFit="cover"
-        source={require("../assets/rectangle-5.png")}
-      />
-      <Image
-        style={styles.ellipseIcon}
-        contentFit="cover"
-        source={require("../assets/ellipse.png")}
-      />
-      <View style={styles.headlineParent}>
-        <Text style={[styles.headline, styles.headlineFlexBox]}>
-          Welcome Back
-        </Text>
-        <Text style={[styles.headline1, styles.headlineFlexBox]}>
-          John Doe!
-        </Text>
-      </View>
-      <View style={[styles.header, styles.headerLayout]}>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.header}>
         <Image
-          style={styles.filterIcon}
-          contentFit="cover"
-          source={require("../assets/filter.png")}
+          source={require("../assets/akariconschevronleft.png")}
+          style={styles.backIcon}
         />
-        <View style={[styles.search, styles.headerLayout]}>
-          <Image
-            style={styles.searchChild}
-            contentFit="cover"
-            source={require("../assets/rectangle-3.png")}
-          />
-          <Image
-            style={styles.iconSearch}
-            contentFit="cover"
-            source={require("../assets/icon-search.png")}
-          />
-          <TextInput
-            style={[styles.search1]}
-            placeholder="Search"
-            placeholderTextColor="rgba(13, 1, 64, 0.6)"
-          />
-        </View>
+        <Text style={styles.title}>Publish a News</Text>
       </View>
-      <ScrollView
-        style={[styles.component32Parent, styles.scrollableSectionPosition]}
-        horizontal={true}
-        showsVerticalScrollIndicator={false}
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.frameScrollViewContent}
-      >
-        <Property1Unselected
-          type8="General"
-          property1UnselectedPosition="unset"
-          property1UnselectedBorderStyle="unset"
-          property1UnselectedBorderColor="unset"
-          property1UnselectedBackgroundColor="#130160"
-          property1UnselectedMarginLeft="unset"
-          type8Color="#fff"
+
+      <View style={styles.footer}>
+        <Image
+          source={require("../assets/ellipse-1.png")}
+          style={styles.footerIcon}
         />
-        <Property1Unselected
-          type8="Health"
-          property1UnselectedPosition="unset"
-          property1UnselectedBorderStyle="solid"
-          property1UnselectedBorderColor="#95969d"
-          property1UnselectedBorderWidth={1}
-          property1UnselectedBackgroundColor="unset"
-          property1UnselectedMarginLeft={10}
-          type8Color="#95969d"
-        />
-        <View style={styles.type8Wrapper}>
-          <Text style={[styles.type8, styles.type8Typo]}>Finance</Text>
-        </View>
-      </ScrollView>
+        <Text style={styles.footerText}>DIV-LINK News</Text>
     </View>
+
+    <View style={styles.formContainer}>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Title</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Write the title of the news here"
+            placeholderTextColor="#aaa6b9"
+            value={title}
+            onChangeText={(text) => setTitle(text)}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Category</Text>
+          <Picker
+            style={styles.picker}
+            selectedValue={category}
+            onValueChange={(itemValue) => setCategory(itemValue)}
+          >
+            <Picker.Item label="Select a category" value="" />
+            <Picker.Item label="Local News" value="652021f9908ee6af777828aa" />
+            <Picker.Item label="Events" value="65277a60cf373daf8cb153d8" />
+            {/* Add more categories as needed */}
+          </Picker>
+        </View>
+
+      
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Content</Text>
+          <TextInput
+            style={[styles.input, styles.contentInput]}
+            placeholder="Content"
+            placeholderTextColor="#aaa6b9"
+            value={content}
+            onChangeText={(text) => setContent(text)}
+            multiline
+          />
+        </View>
+
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>News Image</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter news image URL"
+            placeholderTextColor="#aaa6b9"
+            value={newsImage}
+            onChangeText={(text) => setNewsImage(text)}
+          />
+        </View>
+
+        <TouchableOpacity style={styles.publishButton} onPress={handleAddAppointment}>
+          <Text style={styles.publishText}>Upload News</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.viewNewsButton} onPress={handleViewNews}>
+        <Text style={styles.viewNewsButtonText}>View News</Text>
+      </TouchableOpacity>
+
+    
+
+    </View>
+
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollableSectionScrollViewContent: {
-    flexDirection: "column",
-    paddingBottom: 552,
-    paddingTop: 80,
-    alignItems: "center",
-    justifyContent: "flex-start",
-  },
-  frameScrollViewContent: {
-    flexDirection: "row",
-    paddingTop: 50,
-    alignItems: "center",
-    justifyContent: "flex-start",
-  },
-  scrollableSectionPosition: {
-    left: "50%",
-    top: "50%",
-    position: "absolute",
-  },
-  searchPosition: {
-    left: 0,
-    top: 0,
-  },
-  headlineFlexBox: {
-    display: "flex",
-    color: Color.pureWhite,
-    alignItems: "center",
-    textAlign: "left",
-    width: 254,
-    left: 0,
-    position: "absolute",
-  },
-  headerLayout: {
-    height: 40,
-    position: "absolute",
-  },
-  type8Typo: {
-    fontFamily: FontFamily.medium14,
-    fontWeight: "500",
-    lineHeight: 21,
-    letterSpacing: -0.1,
-    fontSize: FontSize.medium14_size,
-  },
-  scrollableSection: {
-    marginTop: -159.5,
-    marginLeft: -196.5,
-    width: 393,
-    maxWidth: 393,
+  container: {
+    backgroundColor: "#f5f5f5",
+    paddingVertical: 20,
+    paddingHorizontal: 16,
     flex: 1,
-    height: "100%",
-  },
-  menuBarIcon: {
-    bottom: -163,
-    left: -159,
-    width: 736,
-    height: 390,
-    position: "absolute",
-  },
-  myNewsChild: {
-    width: 411.5,
-    height: 221,
-    position: "absolute",
-  },
-  ellipseIcon: {
-    left: 338,
-    width: 54,
-    height: 54,
-    top: 55,
-    position: "absolute",
-  },
-  headline: {
-    alignItems: "center",
-    fontFamily: FontFamily.medium14,
-    fontWeight: "500",
-    lineHeight: 21,
-    letterSpacing: -0.1,
-    fontSize: FontSize.medium14_size,
-    top: 0,
-    display: "flex",
-    color: Color.pureWhite,
-  },
-  headline1: {
-    top: 22,
-    fontSize: FontSize.bold22_size,
-    letterSpacing: -0.3,
-    lineHeight: 26,
-    fontWeight: "700",
-    fontFamily: FontFamily.bold22,
-    alignItems: "center",
-  },
-  headlineParent: {
-    height: 48,
-    width: 254,
-    left: 22,
-    top: 55,
-    position: "absolute",
-  },
-  filterIcon: {
-    top: -146,
-    left: 169,
-    width: 340,
-    height: 340,
-    position: "absolute",
-  },
-  searchChild: {
-    top: -58,
-    left: -62,
-    borderRadius: Border.br_3xs,
-    width: 435,
-    height: 164,
-    position: "absolute",
-  },
-  iconSearch: {
-    top: 8,
-    left: 15,
-    width: 24,
-    height: 24,
-    position: "absolute",
-  },
-  search1: {
-    top: 7,
-    left: 49,
-    fontSize: FontSize.size_xs,
-    fontFamily: FontFamily.dMSansRegular,
-    color: Color.colorDarkgray_100,
-    textAlign: "left",
-    position: "absolute",
-  },
-  search: {
-    width: 280,
-    left: 0,
-    top: 0,
   },
   header: {
-    top: 134,
-    width: 335,
-    left: 22,
-    height: 40,
-  },
-  type8: {
-    color: Color.colorDarkgray_200,
-    textAlign: "left",
-  },
-  type8Wrapper: {
-    borderRadius: Border.br_78xl,
-    borderStyle: "solid",
-    borderColor: Color.colorDarkgray_200,
-    borderWidth: 1,
     flexDirection: "row",
-    justifyContent: "center",
-    paddingHorizontal: Padding.p_xl,
-    paddingVertical: Padding.p_5xs,
-    marginLeft: 10,
     alignItems: "center",
+    paddingTop: 16,
   },
-  component32Parent: {
-    marginTop: -206,
-    marginLeft: -189,
-    width: "100%",
+  
+  backIcon: {
+    paddingEnd: 10,
+    width: 24,
+    height: 24,
   },
-  myNews: {
-    backgroundColor: Color.colorWhitesmoke,
-    height: 893,
-    width: "100%",
-    flex: 1,
+
+  title: {
+    paddingTop: 38,
+    fontSize: 16,
+    fontWeight: "bold",
+    marginLeft: -20,
+  },
+  formContainer: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 8,
+    marginTop: 16,
+    elevation: 4,
+  },
+  inputContainer: {
+    marginBottom: 16,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 4,
+    padding: 8,
+  },
+  picker: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 4,
+    padding: 8,
+  },
+
+  contentInput: {
+    height: 120,
+    textAlignVertical: "top",
+  },
+  footer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 16,
+  },
+  footerIcon: {
+    width: 24,
+    height: 24,
+  },
+  footerText: {
+    fontSize: 16,
+    marginLeft: 8,
+  },
+  publishText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  publishButton: {
+    backgroundColor: "#007bff",
+    borderRadius: 20,
+    padding: 18,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 12,
+  },
+
+  viewNewsButton: {
+    backgroundColor: '#007bff',
+    borderRadius: 20,
+    padding: 18,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  viewNewsButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
+
+
+
 
 export default MyNews;
