30f6fede413415842a082cb9d4551caf1125cb09
IT21071652 <it21071652@my.sliit.lk>
2023-10-27 20:13:19 +0530

    createNewsImage

--- frontend/screens/CreateNews.js
+++ frontend/screens/CreateNews.js
@@ -1,6 +1,16 @@
 import axios from 'axios'; // Import the Axios library
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, Alert, Image,StyleSheet,ScrollView,TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  Alert,
+  Image,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 import * as DocumentPicker from 'expo-document-picker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Picker } from "@react-native-picker/picker";
@@ -11,75 +21,23 @@ const CreateNews = () => {
   const [title, setTitle] = useState('');
   const [category, setCategory] = useState('64eaf5224b77b2ddf24cfabc');
   const [content, setContent] = useState('');
-  const [newsImage, setNewsImage] = useState(null);
+  const [newsImage, setNewsImage] = useState(null); // Used to store the image URL
 
   const selectFile = async () => {
     const result = await DocumentPicker.getDocumentAsync({ type: 'image/*' });
 
     if (result.type === 'success') {
-      setFile(result);
+      setNewsImage(result.uri); // Store the selected image URI
     } else {
       Alert.alert('File selection canceled');
     }
   };
- 
-
-
-  const uploadNews = async () => {
-    if (!file) {
-      Alert.alert('Please select a file first');
-      return;
-    }
-
-    const token = await AsyncStorage.getItem('token');
-
-    if (!token) {
-      console.error('Token is missing in AsyncStorage');
-      return;
-    }
-    console.log(token)
-
-    const formData = new FormData();
-    formData.append('title', title);
-    formData.append('category', category);
-    formData.append('content', content);
-    formData.append('file', {
-      name: file.name,
-      type: file.type,
-      uri: file.uri,
-    });
-
-    try {
-      const response = await axios.post('http://192.168.43.93:8090/api/v1/news/create', formData, {
-        headers: {
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-
-      if (response.status === 201) {
-        const data = response.data;
-        if (data.success) {
-          Alert.alert('News uploaded successfully');
-        } else {
-          Alert.alert('News upload failed');
-        }
-      } else {
-        Alert.alert('Server Error', 'Failed to upload news: ' + response.data.message);
-      }
-    } catch (error) {
-      Alert.alert('Error', 'Failed to upload news: ' + error.message);
-    }
-  };
 
   const handleViewNews = () => {
-    // Use navigation.navigate to navigate to the "NewsView" screen
     navigation.navigate('NewsView');
   };
 
   const handleAddAppointment = async () => {
-  
-
     try {
       const token = await AsyncStorage.getItem('token');
       if (!token) {
@@ -90,16 +48,13 @@ const CreateNews = () => {
         'Authorization': `Bearer ${token}`,
       };
 
-      // Create a request body object with the appointment details
       const requestBody = {
         title: title,
-        category: category, // Replace with your description
+        category: category,
         content: content,
-        newsImage: newsImage, // Make sure selectedTimeSlot is set to a valid value
-
+        newsImage: newsImage, // Use the stored image URI
       };
 
-      console.log(requestBody)
       const response = await axios.post(
         'https://uee123.onrender.com/api/v1/news/create',
         requestBody,
@@ -108,24 +63,12 @@ const CreateNews = () => {
 
       if (response.data.isSuccessful) {
         Alert.alert("Successfully Created");
-        //navigation.navigate("AppoinmentSuccess")
       } else {
         Alert.alert("Failed Try Again: " + response.data.message);
-        //navigation.navigate("Appointments")
       }
     } catch (error) {
-      //console.error('Error creating', error);
-      Alert.alert("Failed to Create" + error.message);
+      Alert.alert("Failed to Create: " + error.message);
     }
-  };
-
-  const HomeScreen = () => {
-    const navigation = useNavigation();
-  
-    const handleAddAppointment = () => {
-      navigation.navigate('NewsView'); // Navigate to the NewsView screen
-    }
-
   };
 
   return (
@@ -138,15 +81,7 @@ const CreateNews = () => {
         <Text style={styles.title}>Publish a News</Text>
       </View>
 
-      <View style={styles.footer}>
-        <Image
-          source={require("../assets/ellipse-1.png")}
-          style={styles.footerIcon}
-        />
-        <Text style={styles.footerText}>DIV-LINK News</Text>
-    </View>
-
-    <View style={styles.formContainer}>
+      <View style={styles.formContainer}>
         <View style={styles.inputContainer}>
           <Text style={styles.label}>Title</Text>
           <TextInput
@@ -172,8 +107,6 @@ const CreateNews = () => {
           </Picker>
         </View>
 
-      
-
         <View style={styles.inputContainer}>
           <Text style={styles.label}>Content</Text>
           <TextInput
@@ -186,16 +119,11 @@ const CreateNews = () => {
           />
         </View>
 
-
         <View style={styles.inputContainer}>
           <Text style={styles.label}>News Image</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Enter news image URL"
-            placeholderTextColor="#aaa6b9"
-            value={newsImage}
-            onChangeText={(text) => setNewsImage(text)}
-          />
+          <TouchableOpacity style={styles.selectFileButton} onPress={selectFile}>
+            <Text style={styles.selectFileButtonText}>Select an Image</Text>
+          </TouchableOpacity>
         </View>
 
         <TouchableOpacity style={styles.publishButton} onPress={handleAddAppointment}>
@@ -203,13 +131,9 @@ const CreateNews = () => {
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.viewNewsButton} onPress={handleViewNews}>
-        <Text style={styles.viewNewsButtonText}>View News</Text>
-      </TouchableOpacity>
-
-    
-
-    </View>
-
+          <Text style={styles.viewNewsButtonText}>View News</Text>
+        </TouchableOpacity>
+      </View>
     </ScrollView>
   );
 };
@@ -226,13 +150,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     paddingTop: 16,
   },
-  
   backIcon: {
     paddingEnd: 10,
     width: 24,
     height: 24,
   },
-
   title: {
     paddingTop: 38,
     fontSize: 16,
@@ -265,23 +187,19 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     padding: 8,
   },
-
   contentInput: {
     height: 120,
     textAlignVertical: "top",
   },
-  footer: {
-    flexDirection: "row",
+  selectFileButton: {
+    backgroundColor: "#007bff",
+    borderRadius: 20,
+    padding: 10,
     alignItems: "center",
-    marginTop: 16,
   },
-  footerIcon: {
-    width: 24,
-    height: 24,
-  },
-  footerText: {
-    fontSize: 16,
-    marginLeft: 8,
+  selectFileButtonText: {
+    color: "white",
+    fontWeight: "bold",
   },
   publishText: {
     color: "white",
@@ -295,7 +213,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     paddingTop: 12,
   },
-
   viewNewsButton: {
     backgroundColor: '#007bff',
     borderRadius: 20,
@@ -308,8 +225,5 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
 });
-
-
-
 
 export default CreateNews;
